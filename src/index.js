5ba049dccfe66facdcdf10d1db3399b665f9dd20
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-07 11:27:28 +0000

    Imported React Routes
    - install react routes
    - defined 2 routes in the index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom'
+
+ReactDOM.render(<Router>
+	<div>
+		<ul>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/contact">Contact</Link></li>
+		</ul>
+		<hr />
+
+		<Route exact path="/" component={Home} />
+		<Route path="/contact" component={Contact} />
+	</div>
+</Router>
+	, document.getElementById('root'));
 registerServiceWorker();
